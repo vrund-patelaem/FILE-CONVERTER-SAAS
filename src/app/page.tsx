1f4589940a3f12f6a334c7892900ba19d53e53e8
Dennis Babych <dennis_babych@gmail.com>
2025-01-31 21:32:46 +0500

    removed old components , added new components...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Features from "@/components/Features";
 import Pricing from "@/components/Pricing";
-import FAQ from "@/components/FAQ";
+import Faq from "@/components/FAQ";
 import CTA from "@/components/CTA";
 import Footer from "@/components/Footer";
 import Comparison from "@/components/Comparison";
@@ -19,7 +19,7 @@ export default function Home() {
       <Comparison/>
         <Features />
         <Pricing />
-        <FAQ />
+        <Faq />
         <CTA />
       </main>
       <Footer />
